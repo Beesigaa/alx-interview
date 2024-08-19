dd366dce40ee9c0e333475b94ff188165cbbbbb3
Beesigaa <adejohadamah@gmail.com>
2024-08-19 08:23:15 -0700

    pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,22 +1,16 @@
 #!/usr/bin/python3
-def pascal_triangle(n):
-    """Generate Pascal's triangle with n rows.
+"""
+0-main
+"""
+pascal_triangle = __import__('0-pascal_triangle').pascal_triangle
 
-    Args:
-        n (int): Number of rows for the triangle.
-
-    Returns:
-        list of lists: Pascal's triangle with n rows.
+def print_triangle(triangle):
     """
-    if n <= 0:
-        return []
+    Print the triangle
+    """
+    for row in triangle:
+        print("[{}]".format(",".join([str(x) for x in row])))
 
-    triangle = [[1]]
-    for i in range(1, n):
-        row = [1]
-        for j in range(1, i):
-            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-        row.append(1)
-        triangle.append(row)
 
-    return triangle
+if __name__ == "__main__":
+    print_triangle(pascal_triangle(5))
